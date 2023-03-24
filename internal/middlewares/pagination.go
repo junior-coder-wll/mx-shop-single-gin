f057357c307989fcd1d8fab9b7298b44b34634ed
吴琳琳 <wulinlin@keyike.com>
2023-03-24 10:37:33 +0800

    first commit

---
+++ internal/middlewares/pagination.go
@@ -0,0 +1,20 @@
+//@Author: wulinlin
+//@Description: 全局分页拦截处理
+//@File:  pagination
+//@Version: 1.0.0
+//@Date: 2023/03/10 13:45
+
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+//
+// PaginateMiddleware
+//  @Description: 全局分页中间件
+//  @return gin.HandlerFunc
+//
+func PaginateMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+
+	}
+}
